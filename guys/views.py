33886a805ff0c45df14ee06129f87843127f3076
zhougit86 <zhou16386@163.com>
2016-12-18 21:59:54 +0800

    render redirect

--- guys/views.py
+++ guys/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response,render,redirect
 from django.http import HttpResponse
 
 
@@ -6,17 +6,21 @@ from forms import GuyForm
 from models import Guy
 
 
+def home_page(request):
+    form=GuyForm(initial={'name': 'OldDriver','email':'Kaiche@gmail.com'})
+    # return render_to_response('guys/new_guy.html',{'form':form})
+    return render(request,'guys/new_guy.html',{'form':form})
 
 def new_guy(request):
-    form=GuyForm(initial={'name': 'Xiaogang','email':'123@gmail.com'})
-    return render_to_response('guys/new_guy1.html',{'form':form})
+    form = GuyForm(request.POST)
+    form.is_valid()
+    cd = form.cleaned_data
+    guy = Guy(name=cd['name'], age=cd['age'], email=cd['email'], phone=cd['phone'], gender=cd['gender'])
+    return redirect("guys")
+
+
+
 
 def guys(request):
-    if request.method == 'POST':
-        form = GuyForm(request.POST)
-        form.is_valid()
-        cd = form.cleaned_data
-        guy = Guy(name=cd['name'],age=cd['age'],email=cd['email'],phone=cd['phone'],gender=cd['gender'])
-        print 'aa'
 
-    return HttpResponse(guy.to_dict())
+    return HttpResponse("save")
