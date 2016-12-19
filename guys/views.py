ad3dcc06793400ff87e0055ed8f46856d48d3d2a
zhouxg2 <zhouxg2@lenovo.com>
2016-12-19 10:18:13 +0800

    guys api

--- guys/views.py
+++ guys/views.py
@@ -11,14 +11,21 @@ def home_page(request):
     # return render_to_response('guys/new_guy.html',{'form':form})
     return render(request,'guys/new_guy.html',{'form':form})
 
-def new_guy(request):
-    form = GuyForm(request.POST)
-    form.is_valid()
-    cd = form.cleaned_data
-    guy = Guy(name=cd['name'], age=cd['age'], email=cd['email'], phone=cd['phone'], gender=cd['gender'])
-    guy.save()
-    return redirect("guys")
+# def new_guy(request):
+#     form = GuyForm(request.POST)
+#     form.is_valid()
+#     cd = form.cleaned_data
+#     guy = Guy(name=cd['name'], age=cd['age'], email=cd['email'], phone=cd['phone'], gender=cd['gender'])
+#     guy.save()
+#     return redirect("guys")
 
 def guys(request):
+    if request.method == 'POST':
+        form = GuyForm(request.POST)
+        form.is_valid()
+        cd = form.cleaned_data
+        guy = Guy(name=cd['name'], age=cd['age'], email=cd['email'], phone=cd['phone'], gender=cd['gender'])
+        guy.save()
+        return redirect("guys")
     guys_list = Guy.objects.all()
     return render(request, 'guys/guys.html', {'guys': guys_list})
