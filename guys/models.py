260a81d72791e35120cb3aa0a44cd75779bcb514
zhouxg2 <zhouxg2@lenovo.com>
2016-12-20 13:25:22 +0800

    DetailView

--- guys/models.py
+++ guys/models.py
@@ -8,5 +8,7 @@ class Guy(models.Model):
     email = models.EmailField(max_length=60)
     phone = models.CharField(max_length=12)
     gender = models.CharField(max_length=2)
+
+    last_access = models.DateTimeField()
     def __repr__(self):
         return self.name
--- guys/views.py
+++ guys/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, JsonResponse
 from django.views import View,generic
 from django.views.decorators.http import require_http_methods,require_GET
 
+from datetime import datetime
 
 from forms import GuyForm
 from models import Guy
@@ -19,7 +20,14 @@ class guy_detail(generic.DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(guy_detail,self).get_context_data(**kwargs)
+        # context ['guy_detail'] = self.model.objects.get(id=self.kwargs['pk'])
         return context
+
+    # def get_object(self):
+    #     object = super(guy_detail, self).get_object()
+    #     object.last_access = datetime.now()
+    #     object.save()
+    #     return object
 
 class guys_view(View):
     model = Guy
@@ -29,7 +37,7 @@ class guys_view(View):
         form = GuyForm(request.POST)
         form.is_valid()
         cd = form.cleaned_data
-        guy = self.model(name=cd['name'], age=cd['age'], email=cd['email'], phone=cd['phone'], gender=cd['gender'])
+        guy = self.model(name=cd['name'], age=cd['age'], email=cd['email'], phone=cd['phone'], gender=cd['gender'],last_access=datetime.now())
         guy.save()
         return redirect("guys")
 
@@ -64,7 +72,7 @@ def guys(request):
         form = GuyForm(request.POST)
         form.is_valid()
         cd = form.cleaned_data
-        guy = Guy(name=cd['name'], age=cd['age'], email=cd['email'], phone=cd['phone'], gender=cd['gender'])
+        guy = Guy(name=cd['name'], age=cd['age'], email=cd['email'], phone=cd['phone'], gender=cd['gender'],last_access=datetime.now())
         guy.save()
         return redirect("guys")
     guys_list = Guy.objects.all()
